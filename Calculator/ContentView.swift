987a288c4fd04e962b78e77edd639fd818bbfc20
opficdev <opficdev@gmail.com>
2024-07-15 00:00:52 +0900

    디바이스가 landscape 모드일 때 앱을 최초 실행하면 potrait모드로 앱이 실행되는 버그 수정

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,58 +7,65 @@
 
 import SwiftUI
 import UIKit
-import Foundation
+import Combine
 
-struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
-
-    func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
+class DeviceOrientationManager: ObservableObject {
+    @Published var orientation: UIDeviceOrientation = UIDevice.current.orientation
+    
+    private var orientationDidChange: AnyCancellable?
+    
+    init() {
+        orientationDidChange = NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+            .compactMap { $0.object as? UIDevice }
+            .map { $0.orientation }
+            .assign(to: \.orientation, on: self)
     }
 }
 
-// A View wrapper to make the modifier easier to use
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(DeviceRotationViewModifier(action: action))
-    }
-}
-
-// An example view to demonstrate the solution
 struct ContentView: View {
-    @State var orientation = UIDeviceOrientation.portrait
-    @State var portraited:Bool = true
-
+    @StateObject private var orientationManager = DeviceOrientationManager()
+    @State private var wasPortrait = true
+    
     var body: some View {
-        Group{
-            if orientation.isLandscape{
-                Landscape().onAppear{
-                    portraited = false
-                }
-            }
-            else if orientation.isFlat {
-                if portraited{
+        VStack {
+            if orientationManager.orientation.isFlat {
+                if wasPortrait{
                     Portrait()
                 }
                 else{
                     Landscape()
                 }
             }
-            else { //Unknown 상태이거나 portrait 상태
-                Portrait().onAppear{
-                    portraited = true
+            else if orientationManager.orientation.isPortrait {
+                Portrait().onAppear(){
+                    wasPortrait = true
                 }
             }
+            else if orientationManager.orientation.isLandscape {
+                Landscape().onAppear(){
+                    wasPortrait = false
+                }
+            }
+            else {
+                Text("Unknown orientation")
+            }
         }
-        .onRotate { newOrientation in
-            orientation = newOrientation
+        .onAppear {
+            UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+        }
+        .onDisappear {
+            UIDevice.current.endGeneratingDeviceOrientationNotifications()
         }
     }
 }
+
+extension UIDeviceOrientation {
+    var isFlat: Bool {
+        return self == .faceUp || self == .faceDown
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
