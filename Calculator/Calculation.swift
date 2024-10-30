f7c3221a74aea0d47128b1d140c429a7184844ab
opfic <opficdev@gmail.com>
2024-10-30 15:41:30 +0900

    10/30 정적 클래스에서 일반 클래스로 변경

--- Calculator/Calculation.swift
+++ Calculator/Calculation.swift
@@ -10,25 +10,22 @@
 import Foundation
 
 
-class Calculation{
-    private static var displayNum:String = "0"  //  화면에 보여지는 수
-    private static var infix_Expr:[String] = ["0"]  // 입력 식(중위) / 입력이 3, + 일 경우 정상적인 연산이 되어야 하므로 ["0"]으로 초기화
-    private static var isNewInput = true    //  숫자가 입력되는 것이 처음인지
-    private static var lastOp = ""  //  마지막으로 입력된 연산자
+class Calculation: ObservableObject {
+    private var displayNum:String = "0"  //  화면에 보여지는 수
+    private var infix_Expr:[String] = ["0"]  // 입력 식(중위) / 입력이 3, + 일 경우 정상적인 연산이 되어야 하므로 ["0"]으로 초기화
+    private var isNewInput = true    //  숫자가 입력되는 것이 처음인지
+    private var lastOp = ""  //  마지막으로 입력된 연산자
     
-    static var num: String {
+    var num: String {
         return displayNum
     }
     
-    static var isEmpty: Bool{
-        if infix_Expr.count == 1{
-            return infix_Expr[0] == "0"
-        }
-        return false
+    var isEmpty: Bool {
+        return infix_Expr == ["0"]
     }
     
     // 연산자 우선순위
-    private static func priority(_ op: String) -> Int{
+    private func priority(_ op: String) -> Int{
         if op == "(" || op == ")"{
             return 0
         }
@@ -42,8 +39,8 @@ class Calculation{
     }
     
     // 입력된 식의 마지막이 숫자인지 판별
-    private static func endsWithNumber() -> Bool{
-        let last = infix_Expr.last  // nil이면 식 infix_Expr이 빈 것
+    private func endsWithNumber() -> Bool{
+       let last = infix_Expr.last  // nil이면 식 infix_Expr이 빈 것
         if last == nil || (last != "+" && last != "-" && last != "*" && last != "/" && last != "(" && last != ")"){
             return true
         }
@@ -51,9 +48,9 @@ class Calculation{
     }
     
     //입력된 수식을 연산자 우선순위에 맞게 후위표기로 변경
-    private static func infix_Postfix() -> [String]{
-        var postfix:[String] = []
-        var stack:[String] = []
+    private func infix_Postfix() -> [String]{
+       var postfix:[String] = []
+       var stack:[String] = []
         for i in infix_Expr{
             if i == "+" || i == "-" || i == "*" || i == "/"{
                 while !stack.isEmpty && (priority(i) <= priority(stack.last!)){
@@ -65,7 +62,7 @@ class Calculation{
                 stack.append(i)
             }
             else if i == ")"{
-                var top_op = stack.popLast()!
+               var top_op = stack.popLast()!
                 while top_op != "("{
                     postfix.append(top_op)
                     top_op = stack.popLast()!
@@ -82,16 +79,16 @@ class Calculation{
     }
     
     //변경된 후위표기 식을 스택을 통해 계산
-    private static func calculation(endWithPrio1: Bool = true) -> String{ // endsWithPrio1: 마지막으로 식에 입력되는 연산자가 + || - 이면 참
-        let postfix = infix_Postfix()
-        var stack:[String] = []
+    private func calculation(endWithPrio1: Bool = true) -> String{ // endsWithPrio1: 마지막으로 식에 입력되는 연산자가 + || - 이면 참
+       let postfix = infix_Postfix()
+       var stack:[String] = []
         for i in postfix{
             if i != "+" && i != "-" && i != "*" && i != "/"{
                 stack.append(i)
             }
             else{
-                let num2 = stack.popLast()!
-                let num1 = stack.popLast()!
+               let num2 = stack.popLast()!
+               let num1 = stack.popLast()!
                 if i == "+"{
                     if !endWithPrio1{
                         stack.append(num2)
@@ -123,7 +120,7 @@ class Calculation{
     }
     
     //키패드에서 숫자 입력받는 함수
-    static func setNum(newNum: String) -> String{
+    func setNum(newNum: String) -> String{
         if displayNum == "0" || displayNum == "오류" || isNewInput{
             displayNum = newNum
             isNewInput = false
@@ -142,7 +139,7 @@ class Calculation{
     }
     
     //clear 버튼
-    static func Clear() -> String{
+    func Clear() -> String{
         displayNum = "0"
         isNewInput = true
         lastOp = ""
@@ -151,7 +148,7 @@ class Calculation{
     }
     
     // +/- 버튼
-    static func Opposite() -> String {
+    func Opposite() -> String {
         if displayNum.first == "-"{
             displayNum.removeFirst()
         }
@@ -163,13 +160,13 @@ class Calculation{
     }
     
     // % 버튼
-    static func Percent() -> String {
+    func Percent() -> String {
         isNewInput = true
         return displayNum
     }
     
     // + 버튼
-    static func Add() -> String {
+    func Add() -> String {
         if displayNum != "오류"{
             if !endsWithNumber(){
                 infix_Expr.removeLast()
@@ -183,7 +180,7 @@ class Calculation{
     }
     
     // - 버튼
-    static func Sub() -> String {
+    func Sub() -> String {
         if displayNum != "오류"{
             if !endsWithNumber(){
                 infix_Expr.removeLast()
@@ -197,7 +194,7 @@ class Calculation{
     }
     
     // * 버튼
-    static func Mul() -> String {
+    func Mul() -> String {
         if displayNum != "오류"{
             if !endsWithNumber(){
                 infix_Expr.removeLast()
@@ -212,7 +209,7 @@ class Calculation{
     }
     
     // / 버튼
-    static func Div() -> String {
+    func Div() -> String {
         if displayNum != "오류"{
             if !endsWithNumber(){
                 infix_Expr.removeLast()
@@ -226,13 +223,13 @@ class Calculation{
     }
     
     // = 버튼
-    static func Equal() -> String {
+    func Equal() -> String {
         if !endsWithNumber(){ //부호로 식이 끝났을 때
             infix_Expr.append(infix_Expr[infix_Expr.count - 2]) //입력이 2, + 일 때 = 을 계속 누르면 해당 연산이 계속 이어져 나가야 함
         }
         if lastOp == "="{   //연속으로 = 를 입력할 시
             lastOp = infix_Expr[infix_Expr.count - 2]
-            let lastNum = infix_Expr.last!
+           let lastNum = infix_Expr.last!
             infix_Expr = [calculation()]
             infix_Expr.append(lastOp)
             infix_Expr.append(lastNum)
@@ -245,7 +242,7 @@ class Calculation{
     }
     
     // 소수점 버튼
-    static func Dot() -> String {
+    func Dot() -> String {
         if displayNum == "오류"{
             displayNum = "0."
             
@@ -258,97 +255,97 @@ class Calculation{
     
     //아래부터 공학계산기 함수
     
-    static func Lbrac(){
+    func Lbrac(){
         
     }
-    static func Rbrac(){
+    func Rbrac(){
         
     }
-    static func Mc(){
+    func Mc(){
         
     }
-    static func Madd(){
+    func Madd(){
         
     }
-    static func Msub(){
+    func Msub(){
         
     }
-    static func Mr(){
+    func Mr(){
         
     }
-    static func Sec(){
+    func Sec(){
         
     }
-    static func X2() -> String {
-        
-        return displayNum
-    }
-    static func X3() -> String{
+    func X2() -> String {
         
         return displayNum
     }
-    static func XY(){
+    func X3() -> String{
+        
+        return displayNum
+    }
+    func XY(){
         
     }
-    static func Ex(){
+    func Ex(){
         
     }
-    static func Tenx(){
+    func Tenx(){
         
     }
-    static func Rev(){
+    func Rev(){
         
     }
-    static func X_2(){
+    func X_2(){
         
     }
-    static func X_3(){
+    func X_3(){
         
     }
-    static func X_y(){
+    func X_y(){
         
     }
-    static func Ln(){
+    func Ln(){
         
     }
-    static func Log10(){
+    func Log10(){
         
     }
-    static func Xf(){
+    func Xf(){
         
     }
-    static func Sin(){
+    func Sin(){
         
     }
-    static func Cos(){
+    func Cos(){
         
     }
-    static func Tan(){
+    func Tan(){
         
     }
-    static func E(){
+    func E(){
         
     }
-    static func EE(){
+    func EE(){
         
     }
-    static func Rad(){
+    func Rad(){
         
     }
-    static func Sinh(){
+    func Sinh(){
         
     }
-    static func Cosh(){
+    func Cosh(){
         
     }
-    static func Tanh(){
+    func Tanh(){
         
     }
-    static func Pi() -> String{
+    func Pi() -> String{
 //        curr_num = NSDecimalNumber(string: "3.141592653589793")
         return displayNum
     }
-    static func Rand(){
+    func Rand(){
         
     }
     
