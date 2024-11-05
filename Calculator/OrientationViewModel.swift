6c818f8793a5eeb62db9da1d7876d8e40ccf444a
최윤진 <opficdev@gmail.com>
2024-11-06 00:32:26 +0900

    11/6 파일변경

---
+++ Calculator/OrientationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrientationViewModel.swift
+//  Calculator
+//
+//  Created by opfic on 11/5/24.
+//
+
+import Foundation
---
+++ Calculator/View/HalfSheet.swift
@@ -0,0 +1,18 @@
+//
+//  HalfSheet.swift
+//  Calculator
+//
+//  Created by opfic on 11/5/24.
+//
+
+import SwiftUI
+
+struct HalfSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HalfSheet()
+}
