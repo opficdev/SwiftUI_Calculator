60033abe857a823f1be6b615bbe9e8ae92f68bc5
opfic <opficdev@gmail.com>
2024-11-09 20:45:06 +0900

    11/9 iOS 18 버전 계산기 기본 모드때 뷰

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -6,70 +6,39 @@
 //
 
 import SwiftUI
-import UIKit
-import Combine
 
-// ViewModel 역할: Orientation 관리와 뷰 상태 관리
-class OrientationViewModel: ObservableObject {
-    private var orientation: UIDeviceOrientation = UIDevice.current.orientation
-    @Published var isPortrait: Bool = true
-    
-    private var orientationDidChange: AnyCancellable?
-    
-    init() {
-        updateOrientation(UIDevice.current.orientation)
-        // Orientation 변경사항을 관찰
-        orientationDidChange = NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
-            .compactMap { $0.object as? UIDevice }
-            .map { $0.orientation }
-            .sink { [weak self] newOrientation in
-                self?.updateOrientation(newOrientation)
-            }
-        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
-    }
-    
-    // Orientation 상태를 업데이트하는 함수
-    private func updateOrientation(_ newOrientation: UIDeviceOrientation) {
-        orientation = newOrientation
-        switch orientation {
-//        case .portrait:
-//            isPortrait = true
-        case .landscapeLeft, .landscapeRight, .portraitUpsideDown:
-                isPortrait = false
-//        case .faceUp, .faceDown, .portraitUpsideDown:
-//            break
-        default:
-            isPortrait = true
-            break
-        }
-    }
-    
-    deinit {
-        UIDevice.current.endGeneratingDeviceOrientationNotifications()
-    }
-}
 
 struct ContentView: View {
     @StateObject private var orientation = OrientationViewModel()
     @StateObject private var viewModel = CalculatorViewModel()
+    @State private var sheetOn = false
+    let ud = UserDefaults.standard
     
     var body: some View {
-        VStack {
-            if orientation.isPortrait {
-                Portrait()
-                    .environmentObject(orientation)
-                    .environmentObject(viewModel)
-            } else {
-                Landscape()
-                    .environmentObject(orientation)
-                    .environmentObject(viewModel)
-            }
+        VStack(alignment: .leading) {
+            Button(action: {
+                sheetOn = true
+            }, label: {
+                Image(systemName: "list.bullet")
+                    .font(.system(size: 25))
+                    .foregroundColor(Color.orange)
+            })
+            .padding(.horizontal)
+            BasicView()
+                .environmentObject(OrientationViewModel())
+                .environmentObject(viewModel)
         }
+        .sheet(isPresented: $sheetOn) {
+            
+        }
+        .background(Color.black)
     }
 }
 
-extension UIDeviceOrientation {
-    var isFlat: Bool {
-        return self == .faceUp || self == .faceDown
-    }
+
+
+#Preview {
+    ContentView()
+        .environmentObject(OrientationViewModel())
+        .environmentObject(CalculatorViewModel())
 }
