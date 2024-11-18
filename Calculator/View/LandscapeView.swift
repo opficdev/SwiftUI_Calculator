ef58aaffa2ba5b54fd8675962608bcefab61d877
opfic <opficdev@gmail.com>
2024-11-18 23:46:07 +0900

    11/18 가로 모드 뷰 구현 예정

---
+++ Calculator/View/LandscapeView.swift
@@ -0,0 +1,18 @@
+//
+//  LandscapeView.swift
+//  Calculator
+//
+//  Created by opfic on 11/18/24.
+//
+
+import SwiftUI
+
+struct LandscapeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandscapeView()
+}
