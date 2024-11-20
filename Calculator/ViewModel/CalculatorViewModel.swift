029869d0d141d18595a60b9de40b5e680bb39e26
opfic <opficdev@gmail.com>
2024-11-20 21:28:45 +0900

    11/20 계산 로직 수정

--- Calculator/ViewModel/CalculatorViewModel.swift
+++ Calculator/ViewModel/CalculatorViewModel.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 class CalculatorViewModel: ObservableObject {
-    @Published var displayNum:String = "0"  //  화면에 보여지는 수
+    @Published var displayNum:String = "0"  //  화면에 보여지는 수(흰색)
+    @Published var history = "" // 회색으로 나타나는 기존 계산식
     @Published var currentAC = true // AC 버튼 on off
     @Published var showSelection = false // 모드 선택 버튼 on off
-    private var infix_Expr:[String] = ["0"]  // 입력 식(중위) / 입력이 3, + 일 경우 정상적인 연산이 되어야 하므로 ["0"]으로 초기화
-    private var isNewInput = true    //  숫자가 입력되는 것이 처음인지
-    
-    var isEmpty: Bool {
-        return infix_Expr == ["0"]
-    }
+    private var infix_Expr:[String] = []  // 입력 식(중위)
+    private var isError = false // 현재 계산 중 에러가 발생했는지
+    private var newNumInput = true // 새로운 숫자가 입력되는지
+    private let fmt = NumberFormatter()
+
     
     // 연산자 우선순위
     private func priority(_ op: String) -> Int {
@@ -34,7 +34,7 @@ class CalculatorViewModel: ObservableObject {
     
     // 입력된 식의 마지막이 숫자인지 판별
     private func endsWithNumber() -> Bool {
-        let last = infix_Expr.last  // nil이면 식 infix_Expr이 빈 것
+        let last = infix_Expr.last
         if last == nil || (last != "+" && last != "-" && last != "*" && last != "/"){
             return true
         }
@@ -73,7 +73,7 @@ class CalculatorViewModel: ObservableObject {
     }
     
     //변경된 후위표기 식을 스택을 통해 계산
-    private func calculation() -> String { // endsWithPrio1: 마지막으로 식에 입력되는 연산자가 + || - 이면 참
+    private func calculation() -> String {
        let postfix = infix_Postfix()
        var stack:[String] = []
         for i in postfix{
@@ -83,33 +83,28 @@ class CalculatorViewModel: ObservableObject {
             else {
                 if let num2 = stack.popLast(), let num1 = stack.popLast() {
                     if i == "+"{
-//                        if !endWithPrio1 {
-//                            stack.append(num2)
-//                            break
-//                        }
                         stack.append((NSDecimalNumber(string: num1).adding(NSDecimalNumber(string: num2))).stringValue)
                     }
                     else if i == "-" {
-//                        if !endWithPrio1{
-//                            stack.append(num2)
-//                            break
-//                        }
                         stack.append((NSDecimalNumber(string: num1).subtracting(NSDecimalNumber(string: num2))).stringValue)
                     }
                     else if i == "*" {
                         stack.append((NSDecimalNumber(string: num1).multiplying(by: NSDecimalNumber(string: num2))).stringValue)
                     }
                     else if i == "/" {
-                        if num1 != "0" {
-                            stack.append((NSDecimalNumber(string: num1).adding(NSDecimalNumber(string: num2))).stringValue)
+                        if num2 != "0" {
+                            stack.append((NSDecimalNumber(string: num1).dividing(by: NSDecimalNumber(string: num2))).stringValue)
                         }
                         else{   // 0으로 나누는 예외처리
-                            return "오류"
+                            isError = true
+                            return "정의되지 않음"
                         }
                     }
                 }
                 else {
-                    return "오류"
+                    isError = true
+                    newNumInput = false
+                    return displayNum //  계산 중 오류 있었을 때인데 다양한 케이스가 존재할 수 있음
                 }
             }
         }
@@ -137,224 +132,270 @@ class CalculatorViewModel: ObservableObject {
 
     
     func handleButtonPress(_ button: BtnType) {
-        if button == .lbrac {
-            if displayNum != "오류" {
-                if endsWithNumber() && !isEmpty {
-                    infix_Expr.append("*")
+        if button == .add {
+            if !isError {
+                if infix_Expr.isEmpty {
+                    infix_Expr.append("0")
                 }
-                infix_Expr.append("(")
+                if !endsWithNumber() {
+                    infix_Expr.removeLast()
+                    displayNum.removeLast()
+                }
+                infix_Expr.append("+")
+                displayNum += "+"
+                newNumInput = true
             }
         }
-        else if button == .rbrac {
-            if displayNum != "오류" {
-                if endsWithNumber() && !isEmpty {
-                    infix_Expr.append("*")
+        else if button == .sub {
+            if !isError {
+                if infix_Expr.isEmpty {
+                    infix_Expr.append("0")
                 }
-                infix_Expr.append(")")
+                if !endsWithNumber() {
+                    infix_Expr.removeLast()
+                    displayNum.removeLast()
+                }
+                infix_Expr.append("-")
+                displayNum += "-"
+                newNumInput = true
             }
         }
-        else if button == .mc {
-            
+        else if button == .mul {
+            if !isError {
+                if infix_Expr.isEmpty {
+                    infix_Expr.append("0")
+                }
+                if !endsWithNumber() {
+                    infix_Expr.removeLast()
+                    displayNum.removeLast()
+                }
+                infix_Expr.append("*")
+                displayNum += "x"
+                newNumInput = true
+            }
         }
-        else if button == .m_add {
-            
+        else if button == .div {
+            if !isError {
+                if infix_Expr.isEmpty {
+                    infix_Expr.append("0")
+                }
+                if !endsWithNumber() {
+                    infix_Expr.removeLast()
+                    displayNum.removeLast()
+                }
+                infix_Expr.append("/")
+                displayNum += "÷"
+                newNumInput = true
+            }
         }
-        else if button == .m_sub {
-            
-        }
-        else if button == .mr {
-            
+        else if button == .equal {
+            if !bracketCorrection() { // 괄호 쌍이 안맞을 경우
+                let bracGap = infix_Expr.filter({ $0 == "(" }).count - infix_Expr.filter({ $0 == ")" }).count
+                if bracGap < 0 {
+                    infix_Expr = Array(repeating: "(", count: -bracGap) + infix_Expr
+                } else {
+                    infix_Expr += Array(repeating: ")", count: bracGap)
+                }
+            }
+            if infix_Expr.isEmpty {
+                return
+            }
+            newNumInput = true
+            history = displayNum
+            print(history)
+            displayNum = calculation()
+            infix_Expr = [displayNum]
         }
         else if button == .allClear {
             displayNum = "0"
-            isNewInput = true
-            infix_Expr = ["0"]
+            isError = false
+            newNumInput = true
+            infix_Expr.removeAll()
         }
         else if button == .clear {
             displayNum.removeLast()
             if displayNum.isEmpty {
                 displayNum = "0"
             }
-        }
-        else if button == .oppo {
-            if displayNum.first == "-" {
-                displayNum.removeFirst()
-            }
-            else{
-                displayNum = "-" + displayNum
-            }
-            isNewInput = true
-        }
-        else if button == .perc {
-            if displayNum != "오류" {
-                
-            }
-            isNewInput = true
-        }
-        else if button == .div {
-            if displayNum != "오류"{
-                if !endsWithNumber() {
-                    infix_Expr.removeLast()
+            if !infix_Expr.isEmpty {
+                let last = infix_Expr.removeLast()
+                if let _num = Int(last) {    // 문자열이 Int 형변환이 되면
+                    let num = String(_num)
+                    if num.count > 1 {  // 숫자가 2자리 이상인 경우
+                        let index = num.index(num.endIndex, offsetBy: -1)
+                        let tmp = String(num[..<index])
+                        infix_Expr.append(tmp)
+                    }
                 }
-                infix_Expr.append("/")
-                isNewInput = true
             }
-        }
-        else if button == ._2nd {
-            
-        }
-        else if button == .x2 {
-            
-        }
-        else if button == .x3 {
-            
-        }
-        else if button == .xy {
-            
-        }
-        else if button == .ex {
-            
-        }
-        else if button == .tenx {
-            
-        }
-        else if button == .mul {
-            if displayNum != "오류" {
-                if !endsWithNumber(){
-                    infix_Expr.removeLast()
-                }
-                infix_Expr.append("*")
-                isNewInput = true
-            }
-        }
-        else if button == .rev {
-            
-        }
-        else if button == .x_2 {
-            
-        }
-        else if button == .x_3 {
-            
-        }
-        else if button == .x_y {
-            
-        }
-        else if button == .ln {
-            
-        }
-        else if button == .log10 {
-            
-        }
-        else if button == .sub {
-            if displayNum != "오류" {
-                if !endsWithNumber() {
-                    infix_Expr.removeLast()
-                }
-                infix_Expr.append("-")
-                isNewInput = true
-            }
-        }
-        else if button == .xf {
-            
-        }
-        else if button == .sin {
-            
-        }
-        else if button == .cos {
-            
-        }
-        else if button == .tan {
-            
-        }
-        else if button == .e {
-            
-        }
-        else if button == .EE {
-            
-        }
-        else if button == .add {
-            if displayNum != "오류" {
-                if !endsWithNumber(){
-                    infix_Expr.removeLast()
-                }
-                infix_Expr.append("+")
-                isNewInput = true
-            }
-        }
-        else if button == .rad {
-            
-        }
-        else if button == .sinh {
-            
-        }
-        else if button == .cosh {
-            
-        }
-        else if button == .tanh {
-            
-        }
-        else if button == .pi {
-            
-        }
-        else if button == .rand {
-            
         }
         else if button == .dot {
-            if displayNum == "오류" {
+            if isError {
                 displayNum = "0."
             }
             else if !displayNum.contains(".") {
                 displayNum += "."
             }
         }
-        else if button == .equal {
-            if !endsWithNumber() { //부호로 식이 끝났을 때
-                infix_Expr.append(infix_Expr[infix_Expr.count - 2]) //입력이 2, + 일 때 = 을 계속 누르면 해당 연산이 계속 이어져 나가야 함
-            }
-            if !bracketCorrection() { // 괄호 쌍이 안맞을 경우
-                let bracGap = infix_Expr.filter({ $0 == "(" }).count - infix_Expr.filter({ $0 == ")" }).count
-                if bracGap < 0 {
-                    infix_Expr = Array(repeating: "(", count: -bracGap) + infix_Expr
+        else if button == .lbrac {
+            if !isError {
+                if endsWithNumber() && !infix_Expr.isEmpty {
+                    infix_Expr.append("*")
                 }
-                else {
-                    infix_Expr += Array(repeating: ")", count: bracGap)
-                }
+                infix_Expr.append("(")
             }
-            displayNum = calculation()
-            isNewInput = true
+        }
+        else if button == .rbrac {
+            if !isError {
+                if endsWithNumber() && !infix_Expr.isEmpty {
+                    infix_Expr.append("*")
+                }
+                infix_Expr.append(")")
+            }
+        }
+        else if button == .mc {
+            // 메모리 클리어 처리
+        }
+        else if button == .m_add {
+            // 메모리 추가 처리
+        }
+        else if button == .m_sub {
+            // 메모리 감소 처리
+        }
+        else if button == .mr {
+            // 메모리 리콜 처리
+        }
+        else if button == .oppo {
+            if !isError {
+                // 값의 부호를 변경하는 코드
+            }
+        }
+        else if button == .perc {
+            if !isError {
+                // 백분율 계산 처리
+            }
+        }
+        else if button == ._2nd {
+            // 2차 기능 처리
+        }
+        else if button == .x2 {
+            // x^2 처리
+        }
+        else if button == .x3 {
+            // x^3 처리
+        }
+        else if button == .xy {
+            // x^y 처리
+        }
+        else if button == .ex {
+            // e^x 처리
+        }
+        else if button == .tenx {
+            // 10^x 처리
+        }
+        else if button == .rev {
+            // 역수 처리
+        }
+        else if button == .x_2 {
+            // x^(1/2) 처리
+        }
+        else if button == .x_3 {
+            // x^(1/3) 처리
+        }
+        else if button == .x_y {
+            // x^(1/y) 처리
+        }
+        else if button == .ln {
+            // 자연 로그 처리
+        }
+        else if button == .log10 {
+            // 상용 로그 처리
+        }
+        else if button == .xf {
+            // 팩토리얼 처리
+        }
+        else if button == .sin {
+            // 사인 처리
+        }
+        else if button == .cos {
+            // 코사인 처리
+        }
+        else if button == .tan {
+            // 탄젠트 처리
+        }
+        else if button == .e {
+            // 자연 상수 e 처리
+        }
+        else if button == .EE {
+            // 과학 표기법 E 처리
+        }
+        else if button == .rad {
+            // 라디안 변환 처리
+        }
+        else if button == .sinh {
+            // 쌍곡선 사인 처리
+        }
+        else if button == .cosh {
+            // 쌍곡선 코사인 처리
+        }
+        else if button == .tanh {
+            // 쌍곡선 탄젠트 처리
+        }
+        else if button == .pi {
+            // 원주율 π 처리
+        }
+        else if button == .rand {
+            // 랜덤 값 처리
         }
         else if button == .emoji {
             
         }
         else { //  숫자 버튼을 눌렀을 때
             if let num: String = button.BtnDisplay.string {
-                if displayNum == "0" || displayNum == "오류" || isNewInput{
+                if isError {
+                    newNumInput = false
+                    infix_Expr = [num]
                     displayNum = num
-                    isNewInput = false
-                    if infix_Expr == ["0"] { //  초기 상태일 때
-                        infix_Expr[0] = num
+                }
+                else if newNumInput {
+                    newNumInput = false
+                    infix_Expr.append(num)
+                    if displayNum == "0" && num == "0" {
+                        newNumInput = true
+                        infix_Expr.removeAll()
+                        displayNum = "0"
+                    }
+                    else if displayNum == "0" {
+                        displayNum = num
                     }
                     else {
-                        infix_Expr.append(num)
+                        displayNum += num
                     }
                 }
                 else {
+                    infix_Expr[infix_Expr.count - 1] += num
                     displayNum += num
-                    infix_Expr[infix_Expr.count - 1] = displayNum
                 }
             }
         }
         
-        if button == .allClear || button == .clear || button == .equal {
+        // 아래 조건문들은 위에 있는 조건문들에 다 일일이 쓰면 currentAC 관련 코드를 넣으면 더러워질 것 같아서 따로 빼놓음
+        if button == .equal || infix_Expr.isEmpty {
             currentAC = true
+        }
+        else if button == .clear {
+            if displayNum == "0" || isError {
+                currentAC = true
+            }
         }
         else if button == .emoji {
             // 이 경우엔 currentAC 값을 유지한다
         }
         else {
-            currentAC = false
+            if !isError {
+                currentAC = false
+            }
         }
     }
-
 }
 
