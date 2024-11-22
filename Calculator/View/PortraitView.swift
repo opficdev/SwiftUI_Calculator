c7cfb1aa30d13bcaa4775a237b754e9a5cd8dd0b
opfic <opficdev@gmail.com>
2024-11-22 23:50:39 +0900

    11/22 가로형 GeometryReader 및 ScrollView 추가

--- Calculator/View/PortraitView.swift
+++ Calculator/View/PortraitView.swift
@@ -12,58 +12,69 @@ struct PortraitView: View {
     @EnvironmentObject var viewModel: CalculatorViewModel
     @Binding var isScientific: Bool
     @State private var btnData: [[BtnType]] = []
-    private let WIDTH = iPhonePointRes.currentDeviceWidth() ?? 0
-    private let HEIGHT = iPhonePointRes.currentDeviceHeight() ?? 0
     
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            VStack(alignment: .trailing, spacing: 0) {
-                Spacer()
-                Text(viewModel.history)
-                    .font(.system(size: 60))
-                    .foregroundColor(Color.gray)
-                    .minimumScaleFactor(0.1)
-                    .lineLimit(1)
-                Text(viewModel.displayExpr)
-                    .font(.system(size: 80))
-                    .foregroundColor(Color.white)
-                    .minimumScaleFactor(0.1)
-                    .lineLimit(1)
-                    .border(Color.white)
-                
-                VStack {
-                    ForEach(btnData, id: \.self) { col in
-                        HStack {
-                            ForEach(col, id: \.self) { button in
-                                Button(action: {
-                                    viewModel.handleButtonPress(button)
-                                }) {
-                                    ButtonLabelView(button: button.BtnDisplay)  // size는 16Pro 기준
-                                        .frame(width: WIDTH / CGFloat(col.count) - 8,   // 8은 .padding()의 기본값
-                                               height: isScientific ? (HEIGHT * 2) / (3 * CGFloat(btnData.count)) - 8 : WIDTH / CGFloat(col.count) - 8,
-                                               alignment: .center)
-                                        .background(button.backgroundColor)
-                                        .cornerRadius(WIDTH / CGFloat(col.count) - 8)
-                                        .foregroundColor(Color.white)
-                                        .font(.system(size: isScientific ? 20 : 35))
+        GeometryReader { geometry in
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack(spacing: 0) {
+                    Spacer()
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 0) {
+                            ForEach(viewModel.history.reversed(), id: \.self) { element in
+                                Text(element)
+                                    .font(.system(size: 40))
+                                    .foregroundColor(Color.gray)
+                            }
+                        }
+                    }
+                    .environment(\.layoutDirection, .rightToLeft)   //  ScrollView를 우측에서 좌측으로
+                    .disabled(true)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 0) {
+                            ForEach(viewModel.displayExpr.reversed(), id: \.self) { element in
+                                Text(element)
+                                    .font(.system(size: 70))
+                                    .foregroundColor(viewModel.isEmpty || viewModel.isContains(string: element) ? Color.white : Color.gray)
+                            }
+                        }
+                    }
+                    .environment(\.layoutDirection, .rightToLeft)
+                    
+                    .disabled(true)    //  조건은 생각해 봐야함
+                    VStack {
+                        ForEach(btnData, id: \.self) { col in
+                            HStack {
+                                ForEach(col, id: \.self) { button in
+                                    Button(action: {
+                                        viewModel.handleButtonPress(button)
+                                    }) {
+                                        ButtonLabelView(button: button.BtnDisplay)  // size는 16Pro 기준
+                                            .frame(width: geometry.size.width / CGFloat(col.count) - 8,   // 8은 .padding()의 기본값
+                                                   height: isScientific ? (geometry.size.height * 2) / (3 * CGFloat(btnData.count)) - 8 : geometry.size.width / CGFloat(col.count) - 8,
+                                                   alignment: .center)
+                                            .background(button.backgroundColor)
+                                            .cornerRadius(geometry.size.width / CGFloat(col.count) - 8)
+                                            .foregroundColor(Color.white)
+                                            .font(.system(size: isScientific ? 20 : 35))
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
-        .onAppear {
-            btnData = isScientific ? scientificBtn + portraitBtn : portraitBtn
-            btnData[isScientific ? 5 : 0][0] = viewModel.currentAC ? .allClear : .clear
-        }
-        .onChange(of: viewModel.currentAC) { newValue in // iOS 17 이상 문법
-            if newValue {
-                btnData[isScientific ? 5 : 0][0] = .allClear
+            .onAppear {
+                btnData = isScientific ? scientificBtn + portraitBtn : portraitBtn
+                btnData[isScientific ? 5 : 0][0] = viewModel.currentAC ? .allClear : .clear
             }
-            else {
-                btnData[isScientific ? 5 : 0][0] = .clear
+            .onChange(of: viewModel.currentAC) { newValue in
+                if newValue {
+                    btnData[isScientific ? 5 : 0][0] = .allClear
+                }
+                else {
+                    btnData[isScientific ? 5 : 0][0] = .clear
+                }
             }
         }
     }
