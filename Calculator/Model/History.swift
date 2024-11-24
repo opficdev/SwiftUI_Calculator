274003af15cfcf3e5ee9fbfcf4d77fcf2f3c53e0
opfic <opficdev@gmail.com>
2024-11-24 22:56:00 +0900

    11/24 계산식/결과를 저장할 History 모델

---
+++ Calculator/Model/History.swift
@@ -0,0 +1,13 @@
+//
+//  History.swift
+//  Calculator
+//
+//  Created by opfic on 11/24/24.
+//
+
+import Foundation
+
+struct History: Hashable {
+    let historyExpr: String
+    let displayExpr: String
+}
