10f334db5c837ad7a6456730d86341dd5b47fc98
opfic <opficdev@gmail.com>
2024-11-27 18:48:50 +0900

    11/27 뷰모델 일부 수정

--- Calculator/ViewModel/HistoryViewModel.swift
+++ Calculator/ViewModel/HistoryViewModel.swift
@@ -9,5 +9,26 @@ import Foundation
 
 class HistoryViewModel: ObservableObject { 
     @Published var showSheet = false
+    @Published var modifyHistory = false
+    @Published var historyData: [String: [History]] = [:]
     
+    var today: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: Date())
+    }
+    
+    func relativeDateString(for dateString: String) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        if let date = dateFormatter.date(from: dateString) {
+            dateFormatter.dateStyle = .long
+            dateFormatter.timeStyle = .none
+            dateFormatter.locale = Locale(identifier: "ko_KR")
+            dateFormatter.doesRelativeDateFormatting = true
+            return dateFormatter.string(from: date)
+        }
+        return nil
+    }
 }
