c45576c4bd3257fe3c9f6e63f11cc3a8970dc87b
opfic <opficdev@gmail.com>
2024-12-02 18:19:37 +0900

    12/2 계산 기록 일부분/전체 삭제 메소드 추가

--- Calculator/ViewModel/HistoryViewModel.swift
+++ Calculator/ViewModel/HistoryViewModel.swift
@@ -14,6 +14,12 @@ class HistoryViewModel: ObservableObject {
     @Published var removeAlert = false
     @Published var historyData: [String: [History]] = [:]
     
+    var selectedCount: Int {
+        return historyData.values.reduce(0) { count, historyItems in
+            count + historyItems.filter { $0.isChecked }.count
+        }
+    }
+    
     var today: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
@@ -32,6 +38,22 @@ class HistoryViewModel: ObservableObject {
             return dateFormatter.string(from: date)
         }
         return nil
+    }
+    
+    func removeHistory() {
+        for keyString in historyData.keys {
+            historyData[keyString] = historyData[keyString]!.filter { !$0.isChecked }
+            if historyData[keyString]!.isEmpty {
+                UserDefaults.standard.removeObject(forKey: keyString)
+                if var dateArr = UserDefaults.standard.array(forKey: "dateArr") as? [String] {
+                    UserDefaults.standard.set(dateArr.filter { $0 != keyString}, forKey: "dateArr")
+                }
+            }
+        }
+        if let encodeDict = try? JSONEncoder().encode(historyData) {
+            UserDefaults.standard.set(encodeDict, forKey: today)
+        }
+        
     }
     
     func removeAllHistory() {
