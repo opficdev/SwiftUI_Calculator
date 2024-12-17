bf27521fc74cd8738a2e2d033d44121ecbdecad9
opfic <opficdev@gmail.com>
2024-12-17 22:14:32 +0900

    2024/12/17 isScientific 변수를 뷰모델로 이동

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -12,47 +12,53 @@ struct ContentView: View {
     @StateObject private var orientation = OrientationViewModel()
     @StateObject private var historyVM = HistoryViewModel()
     @StateObject private var calcVM = CalculatorViewModel()
-    @State private var isScientific = false //  후에 UserDefaults로 옮길 것
+//    @State private var isScientific = false //  후에 UserDefaults로 옮길 것
     
     let ud = UserDefaults.standard
     
     init() {
         if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
             windowScene.windows.forEach { window in
-                window.overrideUserInterfaceStyle = .dark
+                window.overrideUserInterfaceStyle = .dark   //  앱을 다크모드로 설정
             }
         }    
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Button(action: {
-                historyVM.showSheet = true
-            }, label: {
-                Image(systemName: "list.bullet")
-                    .font(.system(size: 22))
-                    .foregroundColor(Color.orange)
-            })
-            .padding()
+        ZStack {
+            VStack(alignment: .leading) {
+                Button(action: {
+                    historyVM.showSheet = true
+                }, label: {
+                    Image(systemName: "list.bullet")
+                        .font(.system(size: 22))
+                        .foregroundColor(Color.orange)
+                })
+                .padding()
 //            if orientation.isPortrait {
-                PortraitView(isScientific: $isScientific)
+                PortraitView()
                     .environmentObject(calcVM)
                     .padding()
                     .padding(.bottom)
-//            }
-//            else {  //가로모드
-//                
-//            }
-           
+//                }
+//                else {  //가로모드
+//
+//                }
+                
+            }
+            .sheet(isPresented: $historyVM.showSheet) {
+                HistoryView()
+                    .environmentObject(historyVM)
+                    .environmentObject(calcVM)
+                    .presentationDetents([.medium, .large])
+                    .presentationDragIndicator(.visible)
+            }
+            .background(Color.black)
+            if calcVM.modeOn {
+                ModeView()
+                    .environmentObject(calcVM)
+            }
         }
-        .sheet(isPresented: $historyVM.showSheet) {
-            HistoryView()
-                .environmentObject(historyVM)
-                .environmentObject(calcVM)
-                .presentationDetents([.medium, .large])
-                .presentationDragIndicator(.visible)
-        }
-        .background(Color.black)
     }
 }
 
---
+++ Calculator/View/ModeView.swift
@@ -0,0 +1,18 @@
+//
+//  ModeView.swift
+//  Calculator
+//
+//  Created by opfic on 12/17/24.
+//
+
+import SwiftUI
+
+struct ModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModeView()
+}
