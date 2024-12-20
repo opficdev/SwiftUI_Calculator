9e64c9a96a232d39b4405d0083788f9395b90110
opfic <opficdev@gmail.com>
2024-12-20 16:36:48 +0900

    historyExpr을 탭 하면 그 기록이 displayExpr로 넘어가도록 추가

--- Calculator/View/PortraitView.swift
+++ Calculator/View/PortraitView.swift
@@ -10,7 +10,10 @@ import UIKit
 
 struct PortraitView: View {
     @EnvironmentObject var calcVM: CalculatorViewModel
-    @State private var btnData: [[BtnType]] = []
+    @State private var btnData: [[BtnType]] = portraitBtn
+    @State private var historyWidth: CGFloat = 0
+    @State private var displayWidth: CGFloat = 0
+    
     
     var body: some View {
         GeometryReader { geometry in
@@ -18,50 +21,107 @@ struct PortraitView: View {
                 Color.black.ignoresSafeArea()
                 VStack(spacing: 0) {
                     Spacer()
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        Text(calcVM.historyExpr.joined())
-                            .font(.system(size: calcVM.btnSize * 0.5))
-                            .foregroundStyle(Color.gray)
-                            .scaleEffect(x: -1, y: 1) // 텍스트 다시 반전
-                    }
-                    .frame(height: calcVM.btnSize * 0.7)  //  ScrollView 내부 Text와 크기 같을 것
-                    .scaleEffect(x: -1, y: 1)
-//                    .disabled()
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        if calcVM.currentAC {   //  계산이 완료됨
-                            if let answer = calcVM.displayExpr.first {
-                                Text(calcVM.setNumberFmt(number: answer, round: true, portrait: true))
-                                    .font(.system(size: calcVM.btnSize))
-                                    .foregroundStyle(Color.white)
-                                    .scaleEffect(x: -1, y: 1) // 텍스트 다시 반전
-                            }
+                    HStack(alignment:. bottom, spacing: 0) {
+                        if calcVM.scientific {
+                            Text("Rad") // 우선 Rad로 고정.
+                                .foregroundStyle(Color.white)
                         }
-                        else {
-                            HStack(spacing: 0) {
-                                ForEach(Array(calcVM.displayExpr.enumerated()), id: \.offset) { index, element in
-                                    Text(
-                                        calcVM.setNumberFmt(
-                                            number: element,
-                                            round: calcVM.displayExpr.count > 1 && index == calcVM.displayExpr.endIndex - 1,
-                                            portrait: true
-                                        )
-                                    )
-                                    .font(.system(size: calcVM.btnSize))
-                                    .foregroundStyle(Color.white)
-                                }
-                                if !calcVM.bracketCorrection() {
-                                    Text(String(repeating: ")", count: calcVM.rbracketAddCount))
-                                        .font(.system(size: calcVM.btnSize))
+                        VStack(spacing: 0) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                if calcVM.currentAC {
+                                    Text(calcVM.historyExpr.joined())
+                                        .font(.system(size: calcVM.btnSize * 0.5))  // 얘도 수정이 있긴 해야하는데 displayExpr보다는 변화량이 작음
                                         .foregroundStyle(Color.gray)
+                                        .scaleEffect(x: -1, y: 1) // 텍스트 다시 반전
+                                        .background(
+                                            GeometryReader { geo in
+                                                Color.clear
+                                                    .onAppear {
+                                                        historyWidth = geo.size.width
+                                                }
+                                                    .onChange(of: calcVM.displayExpr) { _ in
+                                                        historyWidth = geo.size.width
+                                                }
+                                            }
+                                        )
+                                        .onTapGesture {
+                                            calcVM.currentAC = false
+                                        }
                                 }
                             }
-                            .scaleEffect(x: -1, y: 1) // 텍스트 다시 반전
+                            .frame(height: calcVM.btnSize * 0.7)  //  ScrollView 내부 Text와 크기 같을 것
+                            .scaleEffect(x: -1, y: 1)
+                            .disabled(calcVM.scrollUnavailable(innerWidth: historyWidth, outerWidth: geometry.size.width))
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 0) {
+                                    if calcVM.currentAC {   //  계산이 완료됨
+                                        if let answer = calcVM.displayExpr.first {
+                                            Text(calcVM.setNumberFmt(number: answer, round: true, portrait: true))
+                                                .font(.system(size: calcVM.btnSize))
+                                                .foregroundStyle(Color.white)
+                                        }
+                                    }
+                                    else {
+                                        ForEach(Array(calcVM.displayExpr.enumerated()), id: \.offset) { index, element in
+                                            Text(calcVM.setNumberFmt(
+                                                number: element,
+                                                round: !calcVM.historyExpr.isEmpty && calcVM.displayExpr.count > 1 && index == 0,
+                                                portrait: true
+                                            ))
+                                            .foregroundStyle(Color.white)
+                                            .font(.system(size: calcVM.btnSize))
+                                            
+                                        }
+                                        if !calcVM.bracketCorrection() {
+                                            Text(String(repeating: ")", count: calcVM.rbracketAddCount))
+                                                .foregroundStyle(Color.gray)
+                                                .font(.system(size: calcVM.btnSize))
+                                        }
+                                    }
+                                }
+                                .frame(height: calcVM.btnSize)
+                                .scaleEffect(x: -1, y: 1) // 텍스트 다시 반전
+                                .background(
+                                    GeometryReader { geo in
+                                        Color.clear
+                                            .onAppear {
+                                                displayWidth = geo.size.width
+                                        }
+                                            .onChange(of: calcVM.displayExpr) { _ in
+                                                displayWidth = geo.size.width
+                                        }
+                                    }
+                                )
+                            }
+                            .scaleEffect(x: -1, y: 1)
+                            .disabled(calcVM.scrollUnavailable(innerWidth: displayWidth, outerWidth: geometry.size.width))
                         }
                     }
-                    .scaleEffect(x: -1, y: 1)
-//                    .disabled()
                     .padding(.bottom)
+                    
                     VStack {
+                        if calcVM.scientific {
+                            ForEach(scientificBtn, id: \.self) { col in
+                                HStack {
+                                    ForEach(col, id: \.self) { button in
+                                        Button(action: {
+                                            calcVM.handleButtonPress(button)
+                                        }) {
+                                            ButtonLabelView(button: button.BtnDisplay)
+                                                .frame(width: geometry.size.width / CGFloat(col.count) - 8,   // 8은 .padding()의 기본값
+                                                       height: geometry.size.height / CGFloat(btnData.count) * 0.3,
+                                                       alignment: .center
+                                                )
+                                                .background(calcVM.modeOn && button == BtnType.emoji ? Color.clear : button.backgroundColor)
+                                                .cornerRadius(geometry.size.width / CGFloat(col.count) - 8)
+                                                .foregroundStyle(Color.white)
+                                                .font(.system(size: 13))    // 얘는 어차피 공학 모드일때만 나와서 고정함
+                                        }
+                                    }
+                                }
+                            }
+                        }
                         ForEach(btnData, id: \.self) { col in
                             HStack {
                                 ForEach(col, id: \.self) { button in
@@ -70,16 +130,14 @@ struct PortraitView: View {
                                     }) {
                                         ButtonLabelView(button: button.BtnDisplay)
                                             .frame(width: geometry.size.width / CGFloat(col.count) - 8,   // 8은 .padding()의 기본값
-                                                   height: calcVM.scientific ? (geometry.size.height * 2) / (3 * CGFloat(btnData.count)) - 8 :
+                                                   height: calcVM.scientific ? geometry.size.height / CGFloat(btnData.count) * 0.3 :
                                                     geometry.size.width / CGFloat(col.count) - 8,
                                                    alignment: .center
                                             )
                                             .background(calcVM.modeOn && button == BtnType.emoji ? Color.clear : button.backgroundColor)
                                             .cornerRadius(geometry.size.width / CGFloat(col.count) - 8)
                                             .foregroundStyle(Color.white)
-                                            .font(.system(size: (calcVM.scientific ? (geometry.size.width * 2) / (3 * CGFloat(btnData.count)):
-                                                                    geometry.size.width / CGFloat(col.count) - 8) * 0.4)
-                                            )
+                                            .font(.system(size: (calcVM.scientific ? 17 : geometry.size.width * 0.5 / CGFloat(col.count) - 8)))
                                     }
                                     .simultaneousGesture(
                                         LongPressGesture(minimumDuration: 0.5)
@@ -97,20 +155,12 @@ struct PortraitView: View {
                         }
                     }
                 }
-                .onAppear {
-                    btnData = calcVM.scientific ? scientificBtn + portraitBtn : portraitBtn
-                    btnData[calcVM.scientific ? 5 : 0][0] = calcVM.currentAC ? .allClear : .clear
-                }
-                .onChange(of: calcVM.scientific) { _ in
-                    btnData = calcVM.scientific ? scientificBtn + portraitBtn : portraitBtn
-                    btnData[calcVM.scientific ? 5 : 0][0] = calcVM.currentAC ? .allClear : .clear
-                }
                 .onChange(of: calcVM.currentAC) { newValue in
                     if newValue {
-                        btnData[calcVM.scientific ? 5 : 0][0] = .allClear
+                        btnData[0][0] = .allClear
                     }
                     else {
-                        btnData[calcVM.scientific ? 5 : 0][0] = .clear
+                        btnData[0][0] = .clear
                     }
                 }
             }
