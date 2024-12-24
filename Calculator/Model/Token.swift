021352d74f6cc5e2d96dca18980eb166c838e449
opfic <opficdev@gmail.com>
2024-12-24 18:44:12 +0900

    2024/12/24 수식의 기본 요소의 자료형을 담당할 Token 추가

---
+++ Calculator/Model/Token.swift
@@ -0,0 +1,21 @@
+//
+//  Token.swift
+//  Calculator
+//
+//  Created by opfic on 12/23/24.
+//
+//  수식을 구성하는 토큰을 정의
+
+import Foundation
+
+struct Token: Codable, Hashable {
+    let id: UUID
+    var value: String
+    var automatic: Bool //  수식의 구조 때문에 자동으로 추가되었는지(true: 식에 회색으로 처리)
+    
+    init(id: UUID = UUID(), value: String, automatic: Bool = false) {
+        self.id = id
+        self.value = value
+        self.automatic = automatic
+    }
+}
